3002f36f8e8440bc79b699d3dddbdb9a5b9434dc
Amit Philips <a.p360@yahoo.com>
2016-02-02 10:57:26 +0530

    Validations on Blog and Post Models #1
    Issue #1 resolved

--- src/AmpsBlog/Models/Blog.cs
+++ src/AmpsBlog/Models/Blog.cs
@@ -1,13 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmpsBlog.Models
 {
     public class Blog
     {
+        public Blog()
+        {
+            DateCreated = DateTime.UtcNow;
+        }
+
         public int BlogId { get; set; }
+        [Required]
         public string Name { get; set; }
         public string Description { get; set; }
+        [Required]
         public string Url { get; set; }
         public List<Post> Posts { get; set; }
         public DateTime DateCreated { get; set; }
--- src/AmpsBlog/Models/Post.cs
+++ src/AmpsBlog/Models/Post.cs
@@ -1,11 +1,19 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace AmpsBlog.Models
 {
     public class Post
     {
+        public Post()
+        {
+            DateCreated = DateTime.UtcNow;
+        }
+
         public int PostId { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string Content { get; set; }
         public string Tags { get; set; }
         public DateTime DateCreated { get; set; }
