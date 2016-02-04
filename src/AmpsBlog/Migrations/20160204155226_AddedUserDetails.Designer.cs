08a6343cace226af9c9c8dff15749f8b1aaf5e36
Amit Philips <a.p360@yahoo.com>
2016-02-04 21:45:03 +0530

    User now has First, Last Name and Photo saved in the model #8

--- src/AmpsBlog/Migrations/20160204155226_AddedUserDetails.Designer.cs
+++ src/AmpsBlog/Migrations/20160204155226_AddedUserDetails.Designer.cs
@@ -8,9 +8,10 @@ using AmpsBlog.Models;
 namespace AmpsBlog.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20160204155226_AddedUserDetails")]
+    partial class AddedUserDetails
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
             modelBuilder
                 .HasAnnotation("ProductVersion", "7.0.0-rc1-16348")
